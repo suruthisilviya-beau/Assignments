612c85e2524b7df4bddd69730d5d5a9b71390306
suruthisilviya-beau <suruthisilviya@gmail.com>
2025-10-12 12:34:19 +0530

    Day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name="John Doe";
+		int age=30;
+		double salary=55000.75;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		String mobileNumber="9876543210";
+		long pinCode=56001;
+		int luckyNumber=7;		
+		
+
+System.out.println("The Employee name : "+name+"\n"+"Age : "+age+"\n"+"Salary in INR : "+salary+"\n"+"Gender : "+gender+"\n"+"Accept terms : "+acceptTerms+"\n"+"Employee profile Rating : "+rating+"\n"+"Contact Number : "+mobileNumber+"\n"+"Pincode : "+pinCode+"\n"+"Lucky number : "+luckyNumber+"\n");
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+				double browserVersion=100.2;
+				String	browserName="firefox";
+				boolean isVisible=true;
+				short releaseYear=1998;
+				char browserLogo='f';
+System.out.println("The Firefox Browser version is:"+browserVersion+"\n"+"The Browser name is:"+browserName+"\n"+"Is browser visible:"+isVisible+"\n"+"The Release year of the browser:"+releaseYear+"\n"+"The broswer logo is:"+browserLogo+"\n" );
+	//System.out.println(browserLogo+"\n"+browserName);
+	}
+
+}
